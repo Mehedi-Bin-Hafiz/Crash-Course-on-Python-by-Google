fef8093ff09dd705d46dc48ebfb63d5bc8632eae
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-06-21 11:40:09 +0600

    starting  of week 2

---
+++ Week2/Expression_and_varibales/Expression_numbers_Type_conversions.py
@@ -0,0 +1,6 @@
+print(7+8.4)
+
+total = 2048 + 4357+ 97658 + 125 + 8
+files = 5
+average = total / files
+print("The average size is:" + str(average)) #convertion of float to string
---
+++ Week2/Expression_and_varibales/variables.py
@@ -0,0 +1,6 @@
+#area =length *width
+length = 10 # we assign the length variable the value of 10
+width = 20 # we assign the width variable the value of 20
+
+area =length * width # we assign area variable the result of expression
+print(area)
