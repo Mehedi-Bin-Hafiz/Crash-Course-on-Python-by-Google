2f9d23db7746cd6f4fac28b89bb98bfff91f95f9
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-07-09 11:30:12 +0600

    anatomy of a while loop

---
+++ Week3/whileLoop.py
@@ -0,0 +1,8 @@
+x = 0
+
+while x < 5:
+    print("inside while loop the value of x ",x)
+    x = x + 1
+print("outside of the while loop the value of x",x)
+
+''' while loop will continuously execute code depending on the value of a condition. It begins with the keyword while, followed by a comparison to be evaluated, then a colon. On the next line is the code block to be executed, indented to the right. Similar to an if statement, the code in the body will only be executed if the comparison is evaluated to be true. What sets a while loop apart, however, is that this code block will keep executing as long as the evaluation statement is true. Once the statement is no longer true, the loop exits and the next line of code will be executed.'''
