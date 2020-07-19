a2611e2a5242c6e5f2c16bc01cc6d74d9c078b6c
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-07-19 11:02:48 +0600

    String formatting using different method

---
+++ Week4/StringFormatting.py
@@ -0,0 +1,16 @@
+def student_grade(name, grade):
+    res="{} received {}% on the exam".format(name, grade)
+    return res
+
+print(student_grade("Reed", 80))
+print(student_grade("Paige", 92))
+print(student_grade("Jesse", 85))
+
+def to_celsius(x):
+    return (x-32)*5/9
+
+for x in range(0,101,10):
+    print("{:>3} F | {:>6.2f} C".format(x, to_celsius(x)))
+
+
+'''In these two expressions we're using the greater than operator to align text to the right so that the output is neatly aligned. In the first expression we're saying we want the numbers to be aligned to the right for a total of three spaces. In the second expression we're saying we want the number to always have exactly two decimal places and we want to align it to the right at six spaces.'''
