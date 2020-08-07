89860a2e05e4bc00e2c5648036cbf97a3bc79d59
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-08-07 15:13:53 +0600

    List comprehension

--- Week4/List/IteratingOverListAndTuple.py
+++ Week4/List/IteratingOverListAndTuple.py
@@ -34,3 +34,4 @@ def skip_elements(elements):
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))  # Should be ['a', 'c', 'e', 'g']
 print(skip_elements(
     ['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))  # Should be ['Orange', 'Strawberry', 'Peach']
+
